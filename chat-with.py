9771d43cda0e22725a93380db2dd925daea4e910
poojajayasri <pooja.s5462@gmail.com>
2025-02-18 03:20:43 +0530

    Update chat-with.py

--- chat-with.py
+++ chat-with.py
@@ -989,7 +989,7 @@ if st.session_state.tab_selection == "Begin Chat":
                         # response = client.chat.create(
                         #     messages=messages,
                         # )
-                        response = client.Completion.create(
+                        response = client.chat.completions.create(
                             model="gpt-4o-mini",
                             messages=messages,
                         )
